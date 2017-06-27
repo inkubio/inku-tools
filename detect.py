1483b87f9f7bcf40d5463b601c1f739a4a1c66ca
Jonesus <joonas.palosuo@aalto.fi>
2017-06-27 16:49:18 +0300

    Error handling if unable to find faces

--- detect.py
+++ detect.py
@@ -5,13 +5,13 @@ import os
 
 
 def cv2pil(cv2_img):
-    # Convert the cv image to a PIL image
+    # Convert the OpenCV image to a PIL image
     cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
     return Image.fromarray(cv2_img)
 
 
 def detectFace(img, classifier):
-    # Finds faces in grayscale images
+    # Finds faces in grayscale OpenCV images
     faces = classifier.detectMultiScale(img, 1.3, 5)
     for (x, y, w, h) in faces:
         return (x, y, w, h)
@@ -35,38 +35,56 @@ def crop_image(img, x, y, width):
 
 
 def resize(img, size):
-    # Resizes img in place to a square with side length of size
+    # Resizes PIL image in place to a square with side length of size
     size = (size, size)
     img.thumbnail(size, Image.ANTIALIAS)
 
 
 def main():
+    skipped = []
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
     folder = input("Path to images: ")
     savefolder = input("Folder to save to: ")
 
     filepaths = set()
-    for root, dirs, files in os.walk(folder):
+    for root, dirs, files in os.walk(folder):  # Get relative paths to input files
         for filename in files:
             filepaths.add(os.path.join(folder, filename))
 
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
     for file in filepaths:
+        # Open image and convert to grayscale
         img = cv2.imread(file)
         pil_img = cv2pil(img)
         gs_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         area = detectFace(gs_img, face_cascade)
+        if not area:  # Couldn't find a face
+            skipped.append(file)
+            continue
+
         cropped = crop_image(pil_img, area[0], area[1], area[2])
         resize(cropped, 512)
 
         # Show image to user and ask for name
         cropped.show()
         person = input("Who is this: ")
+        if person.lower() in ["skip", "fail", "pass", "no", "none"]:
+            skipped.append(file)
+            os.system('killall display')
+            continue
+
         filename = "_".join(person.split()) + ".jpg"
         cropped.save(os.path.join(savefolder, filename))
 
         # Close shown image
         os.system('killall display')
+
+    if skipped:
+        print("Failed to find a face in following images:")
+        for skip in skipped:
+            print(skip)
+
+    print("Done.")
 
 main()
